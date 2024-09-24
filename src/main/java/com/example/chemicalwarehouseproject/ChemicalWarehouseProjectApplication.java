d65963dcb2046b68f83076d0c3c0fbd9da240c2f
Frederik <fred@hess-pedersen.dk>
2024-09-24 11:04:57 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/chemicalwarehouseproject/ChemicalWarehouseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.chemicalwarehouseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemicalWarehouseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChemicalWarehouseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chemicalwarehouseproject/ChemicalWarehouseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chemicalwarehouseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemicalWarehouseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
